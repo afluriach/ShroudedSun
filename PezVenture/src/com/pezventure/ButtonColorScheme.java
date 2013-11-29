40c06598b083db755d4c22da4289e5236bc37cfd
ant <afluriach@gmail.com>
2013-11-29 17:40:16 -0500

    moved color schemes to class and updated controls

--- PezVenture/src/com/pezventure/ButtonColorScheme.java
+++ PezVenture/src/com/pezventure/ButtonColorScheme.java
@@ -1,6 +1,60 @@
 package com.pezventure;
 
-public class ButtonColorScheme
-{
+import com.badlogic.gdx.graphics.Color;
+import com.pezventure.graphics.Graphics;
 
+public class ButtonColorScheme
+{	
+	public static ButtonColorScheme n64 =
+			new ButtonColorScheme(Graphics.hsva(251f, .8f, .3f, 1f), Graphics.hsva(251f, 1f, .9f, 1f),
+					              Graphics.hsva(49f, .8f, .3f, 1f), Graphics.hsva(49f, 1f, .9f, 1f),
+					              Graphics.hsva(135f, .8f, .3f, 1f), Graphics.hsva(135f, 1f, .9f, 1f),
+					              Graphics.hsva(49f, .8f, .3f, 1f), Graphics.hsva(49f, 1f, .9f, 1f),
+					              Graphics.hsva(251f, 0.01f, .3f, 1f), Graphics.hsva(251f, 0.01f, .7f, 1f));
+	
+	public static ButtonColorScheme xbox = 
+			new ButtonColorScheme(Graphics.hsva(135f, .8f, .3f, 1f), Graphics.hsva(135f, 1f, .9f, 1f),
+								  Graphics.hsva(350f, .8f, .3f, 1f),  Graphics.hsva(350f, 1f, .9f, 1f),
+								  Graphics.hsva(251f, .8f, .3f, 1f), Graphics.hsva(251f, 1f, .9f, 1f),
+								  Graphics.hsva(49f, .8f, .3f, 1f), Graphics.hsva(49f, 1f, .9f, 1f),
+								  Graphics.hsva(251f, 0.01f, .3f, 1f), Graphics.hsva(251f, 0.01f, .7f, 1f));
+	
+	public static ButtonColorScheme gamecube = 
+			new ButtonColorScheme(Graphics.hsva(135f, .8f, .3f, 1f), Graphics.hsva(135f, 1f, .9f, 1f),
+			                      Graphics.hsva(0f, 0f, .3f, 1f), Graphics.hsva(0f, 0f, .9f, 1f),
+			                      Graphics.hsva(350f, .8f, .3f, 1f), Graphics.hsva(350f, 1f, .9f, 1f),
+			                      Graphics.hsva(0f, 0f, .3f, 1f), Graphics.hsva(0f, 0f, .9f, 1f),
+			                      Graphics.hsva(251f, 0.01f, .3f, 1f), Graphics.hsva(251f, 0.01f, .7f, 1f));
+	
+	public static ButtonColorScheme ps = 
+			new ButtonColorScheme(Graphics.hsva(251f, .8f, .3f, 1f), Graphics.hsva(251f, 1f, .9f, 1f),
+								  Graphics.hsva(20f, .8f, .3f, 1f), Graphics.hsva(20f, 1f, .9f, 1f),
+							  	  Graphics.hsva(350f, .8f, .3f, 1f), Graphics.hsva(350f, 1f, .9f, 1f),
+								  Graphics.hsva(135f, .8f, .3f, 1f), Graphics.hsva(135f, 1f, .9f, 1f),
+								  Graphics.hsva(251f, 0.01f, .3f, 1f), Graphics.hsva(251f, 0.01f, .7f, 1f));
+	
+	Color adark, alight;
+	Color bdark, blight;
+	Color xdark, xlight;
+	Color ydark, ylight;
+	
+	Color dpaddark, dpadlight;
+
+	public ButtonColorScheme(Color adark, Color alight, Color bdark,
+			Color blight, Color xdark, Color xlight, Color ydark, Color ylight,
+			Color dpaddark, Color dpadlight) {
+		this.adark = adark;
+		this.alight = alight;
+		this.bdark = bdark;
+		this.blight = blight;
+		this.xdark = xdark;
+		this.xlight = xlight;
+		this.ydark = ydark;
+		this.ylight = ylight;
+		this.dpaddark = dpaddark;
+		this.dpadlight = dpadlight;
+	}
+	
+	
+		
 }
--- PezVenture/src/com/pezventure/Controls.java
+++ PezVenture/src/com/pezventure/Controls.java
@@ -33,57 +33,8 @@ public class Controls
 	public static final int dpadThickness = 60;
 	public static final int dpadSectionLength = dpadLength/2 - dpadThickness/2;
 	public static final int dpadSectionOffset = dpadLength/2 + dpadThickness/2;
-
-// xbox style button coloring
-//	public static final Color buttonAdark = Graphics.hsva(135f, .8f, .3f, 1f);
-//	public static final Color buttonBdark = Graphics.hsva(350f, .8f, .3f, 1f);
-//	public static final Color buttonXdark = Graphics.hsva(251f, .8f, .3f, 1f);
-//	public static final Color buttonYdark = Graphics.hsva(49f, .8f, .3f, 1f);
-//	
-//	public static final Color buttonAlight = Graphics.hsva(135f, 1f, .9f, 1f);
-//	public static final Color buttonBlight = Graphics.hsva(350f, 1f, .9f, 1f);
-//	public static final Color buttonXlight = Graphics.hsva(251f, 1f, .9f, 1f);
-//	public static final Color buttonYlight = Graphics.hsva(49f, 1f, .9f, 1f);
-
-//n64 style coloring	
-
-//	public static final Color buttonAdark = Graphics.hsva(251f, .8f, .3f, 1f);
-//	public static final Color buttonAlight = Graphics.hsva(251f, 1f, .9f, 1f);
-//	public static final Color buttonXdark = Graphics.hsva(135f, .8f, .3f, 1f);
-//	public static final Color buttonXlight = Graphics.hsva(135f, 1f, .9f, 1f);
-//	public static final Color buttonBdark = Graphics.hsva(49f, .8f, .3f, 1f);
-//	public static final Color buttonBlight = Graphics.hsva(49f, 1f, .9f, 1f);
-//	public static final Color buttonYdark = Graphics.hsva(49f, .8f, .3f, 1f);
-//	public static final Color buttonYlight = Graphics.hsva(49f, 1f, .9f, 1f);
-
-//gamecube style coloring
-
-//	public static final Color buttonAdark = Graphics.hsva(135f, .8f, .3f, 1f);
-//	public static final Color buttonXdark = Graphics.hsva(350f, .8f, .3f, 1f);
-//	public static final Color buttonBdark = Graphics.hsva(0f, 0f, .3f, 1f);
-//	public static final Color buttonYdark = Graphics.hsva(0f, 0f, .3f, 1f);
-//	
-//	public static final Color buttonAlight = Graphics.hsva(135f, 1f, .9f, 1f);
-//	public static final Color buttonXlight = Graphics.hsva(350f, 1f, .9f, 1f);
-//	public static final Color buttonBlight = Graphics.hsva(0f, 0f, .9f, 1f);
-//	public static final Color buttonYlight = Graphics.hsva(0f, 0f, .9f, 1f);
-
-//ps2 style controls
 	
-	public static final Color buttonYdark = Graphics.hsva(135f, .8f, .3f, 1f);
-	public static final Color buttonBdark = Graphics.hsva(20f, .8f, .3f, 1f);
-	public static final Color buttonAdark = Graphics.hsva(251f, .8f, .3f, 1f);
-	public static final Color buttonXdark = Graphics.hsva(350f, .8f, .3f, 1f);
-	
-	
-	public static final Color buttonXlight = Graphics.hsva(350f, 1f, .9f, 1f);
-	public static final Color buttonYlight = Graphics.hsva(135f, 1f, .9f, 1f);
-	public static final Color buttonBlight = Graphics.hsva(20f, 1f, .9f, 1f);
-	public static final Color buttonAlight = Graphics.hsva(251f, 1f, .9f, 1f);
-
-	
-	public static final Color dpadDark = Graphics.hsva(251f, 0.01f, .3f, 1f);
-	public static final Color dpadLight = Graphics.hsva(251f, 0.01f, .7f, 1f);
+	static final ButtonColorScheme colors = ButtonColorScheme.xbox;
 	
 	//touch
 	boolean touchControls;
@@ -172,7 +123,7 @@ public class Controls
 	
 	public void drawDpadSegment(ShapeRenderer shapeRenderer, boolean pressed, Rectangle segment)
 	{
-		shapeRenderer.setColor(pressed ? dpadDark : dpadLight);
+		shapeRenderer.setColor(pressed ? colors.dpaddark : colors.dpadlight);
 		
 		shapeRenderer.rect(segment.x, segment.y, segment.width, segment.height);
 	}
@@ -181,22 +132,22 @@ public class Controls
 	{
 		shapeRenderer.begin(ShapeType.Filled);
 		
-		drawButtonOuter(shapeRenderer, buttonAlight, buttonA);
-		drawButtonOuter(shapeRenderer, buttonBlight, buttonB);
-		drawButtonOuter(shapeRenderer, buttonXlight, buttonX);
-		drawButtonOuter(shapeRenderer, buttonYlight, buttonY);
+		drawButtonOuter(shapeRenderer, colors.alight, buttonA);
+		drawButtonOuter(shapeRenderer, colors.blight, buttonB);
+		drawButtonOuter(shapeRenderer, colors.xlight, buttonX);
+		drawButtonOuter(shapeRenderer, colors.ylight, buttonY);
 		
-		drawButtonInner(shapeRenderer,a, buttonAlight, buttonAdark, buttonA);
-		drawButtonInner(shapeRenderer,b, buttonBlight, buttonBdark, buttonB);
-		drawButtonInner(shapeRenderer,x, buttonXlight, buttonXdark, buttonX);
-		drawButtonInner(shapeRenderer,y, buttonYlight, buttonYdark, buttonY);
+		drawButtonInner(shapeRenderer,a, colors.alight, colors.adark, buttonA);
+		drawButtonInner(shapeRenderer,b, colors.blight, colors.bdark, buttonB);
+		drawButtonInner(shapeRenderer,x, colors.xlight, colors.xdark, buttonX);
+		drawButtonInner(shapeRenderer,y, colors.ylight, colors.ydark, buttonY);
 		
 		drawDpadSegment(shapeRenderer, up, dpadUp);
 		drawDpadSegment(shapeRenderer, right, dpadRight);
 		drawDpadSegment(shapeRenderer, down, dpadDown);
 		drawDpadSegment(shapeRenderer, left, dpadLeft);
 
-		shapeRenderer.setColor(dpadDark);
+		shapeRenderer.setColor(colors.dpaddark);
 		shapeRenderer.rect(dpadCenter.x, dpadCenter.y, dpadCenter.width, dpadCenter.height);
 		
 		shapeRenderer.end();
