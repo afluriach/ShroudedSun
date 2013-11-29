6455f7075126db5db5070a165ffbbf642f91e938
ant <afluriach@gmail.com>
2013-11-29 17:39:53 -0500

    refactor gamestate -> save state. new game state interface.

--- PezVenture/src/com/pezventure/GameState.java
+++ PezVenture/src/com/pezventure/GameState.java
@@ -1,26 +1,6 @@
 package com.pezventure;
 
-import java.util.TreeMap;
-
-import com.badlogic.gdx.utils.Json;
-import com.badlogic.gdx.utils.Json.Serializable;
-import com.badlogic.gdx.utils.JsonValue;
-import com.badlogic.gdx.utils.OrderedMap;
-
-//represents persistent game state that can be loaded and stored
-public class GameState implements Serializable
+public interface GameState
 {
-	TreeMap<String, JsonValue> areaState = new TreeMap<String, JsonValue>();
-			
-	@Override
-	public void write(Json json)
-	{
-		json.toJson(this);
-	}
-
-	@Override
-	public void read(Json json, JsonValue jsonData)
-	{
-		json.fromJson(GameState.class, jsonData.toString());
-	}
+	
 }
